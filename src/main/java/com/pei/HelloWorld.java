65c80bd36173c502436f03a47b9c54a8891b36ff
JoanneHsia <joanneh1109@gmail.com>
2021-03-30 16:28:23 +0800

    box

--- src/main/java/com/pei/HelloWorld.java
+++ src/main/java/com/pei/HelloWorld.java
@@ -9,15 +9,19 @@ public class HelloWorld {
 //        stu.print();
 //        int[] scores = {50, 80, 35, 90};
 //        int[] ss = new int[5];
+        /*
         Student[] students = {
                 new Student("1101", "Eric", 60, 80),
                 new Student("1102", "Hank", 60, 80),
                 new Student("1103", "Jane", 60, 80),
                 new Student("1106", "Jack", 60, 80)
         };
+
         for (int i = 0; i < students.length; i++) {
             students[i].print();
         }
+
+         */
 
     }
 }
